db7f2c112a84a09162056ddc2fbef1e7d0e38c31
Luke0125 <ghok1003@naver.com>
2026-02-18 17:41:29 +0900

    commit

--- 2026_cv/softmax.py
+++ 2026_cv/softmax.py
@@ -5,15 +5,6 @@ def softmax_loss_vectorized(W, x, y, reg):
     loss = 0.0
     dW = np.zeros_like(W)
 
-    num_classes = W.shape[0]
-    num_train = x.shape[0]
-    print(num_classes.shape)
-
-    return loss, dW
-    '''
-    loss = 0.0
-    dW = np.zeros_like(W)
-
     num_classes = W.shape[1] 
     num_train = x.shape[0] 
 
@@ -32,5 +23,20 @@ def softmax_loss_vectorized(W, x, y, reg):
     dW = np.dot(x.T, p)
     dW = dW / num_train + 2 * reg * W
 
-    # [핵심!] 이 부분이 반드시 있어야 합니다.
+    return loss, dW
+    '''
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    num_classes = W.shape[1]
+    num_train = x.shape[0]
+
+    # Forward Pass
+    scores = np.dot(x, W) # (500, 10)
+    # axis=1에 해당하는 column을 축소하면서 진행해라!(column 방향으로 진행하라!) -> 같은 행 중 최댓값을 찾아라!
+    scores -= np.max(scores, axis=1, keepdims=True) # (500, 1)
+    print(scores.shape)
+
+
+
     return loss, dW
